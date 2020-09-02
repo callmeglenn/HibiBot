c99f5f078b7dd369bcdddc87c3b40e68dc9b54be
HibikiSpring <ericgankayhong@outlook.com>
2020-09-02 18:53:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PREFIX = 'h!';
 bot.on('ready', () =>{
     console.log('Roger, Hibiki, heading out.');
     
-    client.user.setPresence({
+    client.user.setActivity({
         status: "online",
         game: {
             name: "h!help",
