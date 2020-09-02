49efaf5cfcc449612f051a5a1d3915b1852c902c
HibikiSpring <ericgankayhong@outlook.com>
2020-09-02 18:24:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,43 +15,6 @@ bot.on('ready', () =>{
 
 })
 
-module.exports.run = async (bot, message, args) => {
-    let helpArray = message.content.split(" ");
-    let helpArgs = helpArray.slice(1);
-
-    if(helpArgs[0]) {
-        var embed = new Discord.MessageEmbed()
-            .setAuthor('Commander, your orders please.')
-            .setDescription('```help | pic```')
-            .addFields({ name: 'Prefix', value: '```h!```', inline: true})
-            .setColor('#00FFF3')
-    }
-    let command = helpArgs[0];
-
-    if(bot.commands.has(command)) {
-        
-        command = bot.commands.get(command);
-        var embed = new Discord.MessageEmbed()
-        .setAuthor(`${command.config.name} Command`)
-        .setDescription(`
-        - **Command's Description** __${command.config.description || "There is no Description for this command."}__
-        - **Command's Usage:** __${command.config.usage || "No Usage"}__
-        - **Command's Permissions:** __${command.config.accessableby || "Members"}__
-        - **Command's Aliases:** __${command.config.aliases || "No Aliases"}__
-        `)
-        .setColor('#2EFF00')
-
-    message.channel.send(embed);
-}}    
-
-module.exports.config = {
-    name: "help",
-    description: "",
-    usage: "h!help",
-    accessableby: "Members",
-    aliases: []
-}
-
 bot.on('message', message => {
  
     let args = message.content.substring(PREFIX.length).split(" ");
