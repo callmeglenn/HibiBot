13a4f5b3b1210f426a4a5e06ce4622e89e194846
HibikiSpring <ericgankayhong@outlook.com>
2020-09-02 22:38:02 +0800

    First

--- index.js
+++ index.js
@@ -1,107 +1,25 @@
-const Discord = require ('discord.js');
-const bot = new Discord.Client();
+const Discord = require('discord.js');
+const botsettings = require('./botsettings.json');
 
-const cheerio = require('cheerio');
+const bot = new Discord.Client({disableEveryone: true});
 
-const request = require('request');
-
-const config = require('./config.json')
-const command = require('./command')
-
-bot.login(process.env.token);
-
-const PREFIX = 'h!';
-
-bot.on('ready', () =>{
-    console.log('Roger, Hibiki, heading out.');
-    bot.user.setActivity('h!help', {type: 'STREAMING'}).catch(console.error);
-
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online`)
+    bot.user.setActivity("h!help", {type: "STREAMING", url:"https://twitch.tv/HibikiSpring"});
 })
 
-bot.on('message', message => {
- 
-    let args = message.content.substring(PREFIX.length).split(" ");
- 
-    switch (args[0]) {
-        case 'pic':
-        image(message);
- 
-        break;
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}info`){
+        return message.channel.send("HibiBot by Hibiki#2879")
     }
-});
- 
+    
+})
 
-function image(message){
- 
-    var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "Hibiki (Kantai Collection)",
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    };
- 
- 
- 
- 
- 
-    request(options, function(error, response, responseBody) {
-        if (error) {
-            return;
-        }
- 
- 
-        $ = cheerio.load(responseBody);
- 
- 
-        var links = $(".image a.link");
- 
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-       
-        console.log(urls);
- 
-        if (!urls.length) {
-           
-            return;
-        }
- 
-        // Send result
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-    });
- 
-}
-command(bot, 'embed', (message) => {
-
-    const embed = new Discord.MessageEmbed()
-      .setTitle('Commander, your orders please.')
-      .setURL('https://invite.gg/Hibiki')
-      .setAuthor(message.author.username)
-      .setImage(logo)
-      .setThumbnail(logo)
-      .setFooter('HibiBot by Hibiki#2879')
-      .setColor('#00AAFF')
-      .addFields(
-        {
-          name: 'Field 1',
-          value: 'Hello world',
-          inline: true,
-        },
-        {
-          name: 'Field 2',
-          value: 'Hello world',
-          inline: true,
-        },
-        {
-          name: 'Field 3',
-          value: 'Hello world',
-          inline: true,
-        },
-        {
-          name: 'Field 4',
-          value: 'Hello world',
-        }
-      )
-
-    message.channel.send(embed)
-  })
+bot.login(botsettings.token);
