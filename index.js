8cb76ffbc71ad664d1a1cd4c6379afd9a2207a5d
HibikiSpring <ericgankayhong@outlook.com>
2020-09-02 18:15:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,24 @@ module.exports.run = async (bot, message, args) => {
             .addFields({ name: 'Prefix', value: '```h!```', inline: true})
             .setColor('#00FFF3')
     }
-}
+    let command = helpArgs[0];
+
+    if(bot.commands.has(command)) {
+        
+        command = bot.commands.get(command);
+        var embed = new Discord.MessageEmbed()
+        .setAuthor(`${command.config.name} Command`)
+        .setDescription(`
+        - **Command's Description** __${command.config.description || "There is no Description for this command."}__
+        - **Command's Usage:** __${command.config.usage || "No Usage"}__
+        - **Command's Permissions:** __${command.config.accessableby || "Members"}__
+        - **Command's Aliases:** __${command.config.aliases || "No Aliases"}__
+        `)
+        .setColor('#2EFF00')
+
+    message.channel.send(embed);
+}}    
+
 
 bot.on('message', message => {
  
