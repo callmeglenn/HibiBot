974d6b8aaf8ff83b4ef47a947caea58329b5965e
HibikiSpring <ericgankayhong@outlook.com>
2020-09-03 04:13:42 +0800

    First

---
+++ commands/image.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('Here is the Available Commands to use:')
+        .setURL('http://invite.gg/Hibiki')
+        .setAuthor('Commander, your orders please.')
+        .setDescription('These commands will have different results.')
+        .setColor('#FF2D00')
+        .setFooter('HibiBot by Hibiki#2879')
+        .addFields(
+            { name: 'Hibiki (KanColle)', value: 'h!hibiki', inline: true},
+            { name: 'Verniy (KanColle)', value: 'h!verniy', inline: true},
+            { name: 'Hibiki (Kantai Collection)', value: 'h!hibikikc'},
+            { name: 'Verniy (Kantai Collection)', value: 'h!verniykc'}
+        )
+        .setTimestamp()
+
+
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+        name: "img",
+        description: "hibiki, hibikikc, verniy, verniykc",
+        usage: "h!img",
+        accessableby: "Members",
+        aliases: []
+    }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const botsettings = require('./botsettings.json');
+const cheerio = require('cheerio');
+const request = require('request');
+const prefix = `h!`;
 
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -8,18 +11,276 @@ bot.on("ready", async () => {
     bot.user.setActivity("h!help", {type: "STREAMING", url:"https://twitch.tv/HibikiSpring"});
 })
 
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
 
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ")
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
-    let args = messageArray.slice(1);
+    let args = message.content.substring(message.content.indexOf(' ')+1);
 
-    if(cmd === `${prefix}info`){
-        return message.channel.send("HibiBot by Hibiki#2879")
-    }
-    
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
 })
+
+bot.on('message', message => {
+ 
+    let args = message.content.substring(prefix.length).split(" ");
+ 
+    switch (args[0]) {
+        case 'hibikikc':
+        image(message);
+ 
+        break;
+    }
+ 
+});
+ 
+function image(message){
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "Hibiki (Kantai Collection)",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
+bot.on('message', message => {
+ 
+    let args = message.content.substring(prefix.length).split(" ");
+ 
+    switch (args[0]) {
+        case 'verniykc':
+        image(message);
+ 
+        break;
+    }
+ 
+});
+ 
+function image(message){
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "Verniy (Kantai Collection)",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
+bot.on('message', message => {
+ 
+    let args = message.content.substring(prefix.length).split(" ");
+ 
+    switch (args[0]) {
+        case 'hibiki':
+        image(message);
+ 
+        break;
+    }
+ 
+});
+ 
+function image(message){
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "Hibiki (KanColle)",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
+bot.on('message', message => {
+ 
+    let args = message.content.substring(prefix.length).split(" ");
+ 
+    switch (args[0]) {
+        case 'verniy':
+        image(message);
+ 
+        break;
+    }
+ 
+});
+ 
+function image(message){
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "Verniy (KanColle)",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+ 
+ 
+ 
+ 
+ 
+}
 
 bot.login(botsettings.token);
