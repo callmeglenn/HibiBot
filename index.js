c3ec97720dac635b940ba19c7da2e86db5ab8888
HibikiSpring <ericgankayhong@outlook.com>
2020-09-05 11:18:41 +0800

    First

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const personalityResponses = ["Characterized in the anime by her vacant attitude
 const triviaResponses = ["Hibiki means echo, Верный (Verniy) means loyal and her final name, Декабрист (Dekabrist) is a tribute to the Decembrist revolt.", "Hibiki was first carried by the twenty-first ship of the first Kamikaze class in 1906.", "Верный was first carried by a submarine class in the old USSR.", "Hibiki's present incarnation exists as the lead ship of Hibiki-class Ocean Surveillance Ship, AOS-5201 JDS Hibiki of Japan Maritime Self-Defense Force.", "Received her Kai Ni (Верный) on 11/09/2013.", "Shunsaku Kudō was transferred to Hibiki after his humanitarian rescue of enemy personnel while captain of Ikazuchi.", "She was retired from service on February 20 1953 and sunk as a target by the Soviet Naval Aviation team sometime in the 1970's at Karamzina Island near Vladivostok.", "After being discovered in 2012, her wreckage is now a popular diving site. Seasonal diving tours to her wreckage are available."];
 const quotesResponses = ["I'm Hibiki. I'm also referred to as The Phoenix from my exploits.", "I'm Hibi— Верный. It's a name that has the meaning of Faithful (in Russian).", "Surviving till the very end among the many special-class destroyers was I, Hibiki. After experiencing several battles, I was unable to accompany the Yamato Surface Special Attack Force due to repairs. I was handed over to the Soviet Union as a prize of war, and was given a name that means Faithful (in Russian).", "Commander, what is it?", "Commander, are you alright?", "Is it fine if I return soon?", "Commander, your orders please.", "до свидания.", "It's all right. Even if I am alone.", "Commander, aren't you tired?", "Commander, things like affection and love... what are they...? Eh? You'll teach me?", "It appears there is a message for you, Commander.", "	Roger, Hibiki, heading out.", "Roger, Верный, heading out.", "Хорошо... I can feel the power in this.", "Хорошо... I can trust in this.", "Hm, this is great... Спасибо.", "This is great... Спасибо.", "That'll work.", "Ура!", "Спасибо.", "I'll go patch myself up.", "The secret of The Phoenix lies in the timing of its repairs as well.", "I heard the repairs of the ship are completed.", "It seems like fresh forces have been added.", "The fleet has returned. Thanks for the hard work.", "My reputation as The Phoenix isn't just for show. Sortieing.", "My name of Faithful isn't just for show. Sortieing.", "Well then, shall we do it?", "How futile.", "Too slow.", "Хорошо.", "Victory? It has a nice ring to it. I don't hate it.", "Ugh...", "I won't sink...", "As expected, this is... embarrassing.", "My final name is Verniy... до свидания.", "My true name is Hibiki... до свидания... Goodbye."];
 const seasonalquotesResponses = ["Commander's hand... is warm. No, that's... It's very handy in Russia.", "Commander, I brought some Russian chocolate. What makes that Russian? It's a secret.", "Spring, huh. Spring is nice. A winter will always be followed by spring. Хорошо (Very good).", "Commander, what's this? Return gift? Thank you. I'll take it.", "Commander, it's the fleet's and our 6thDesDiv's third anniversary. Splendid. As expected, this is... pleasant.", "Inazuma's teru-teru bouzu, looks nice. It's cute. As for Akatsuki's... what may it be? A monster?", "Commander's hand... it's warm. No, that's... It's a gifted hand to have in Russia.", "Akatsuki, you have to build the breakwater taller or else... Ah... it's fine, we can always build it again. Let me help you."]
+const secretResponses = ["Ура! You found a secret command, message Hibiki#2879 to claim your reward!"]
 
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -439,6 +440,28 @@ bot.on('message', function (message) {
             var response = seasonalquotesResponses [Math.floor(Math.random()*seasonalquotesResponses .length)];
 
             message.channel.send(response).then().catch(console.error);
+            break;
+        default:
+            break;
+    }
+});
+
+// Hibiki's Secret
+bot.on('message', function (message) {
+    // It is considered bad practice to let your bot react to other bots.
+    if (message.author.bot) return;
+
+    // If a message doesn't start with your bot's prefix, don't bother going through the cases.
+    if (!message.content.startsWith("h!")) return;
+
+    // Args length check. #1 is the command, #2 and higher are the arguments
+    var args = message.content.substring("h!".length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "secret":
+            var response = secretResponses [Math.floor(Math.random()*secretResponses .length)];
+
+            message.author.send(response).then().catch(console.error);
             break;
         default:
             break;
