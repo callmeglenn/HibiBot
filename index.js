7c76cf286575b9bbc67b0edf659e6c1f299c05f5
HibikiSpring <70623714+HibikiSpring@users.noreply.github.com>
2021-03-03 17:08:40 +0800

    First

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'], fetchAllMembers: true })
+require('mongoose')
+const mongo = require('./mongo')
 
 const config = require('./config')
 client.config = config
@@ -8,6 +10,10 @@ client.commands = new Discord.Collection()
 const { registerEvents, registerCommands } = require('./utils/registry')
 client.once('ready', async function () {
     console.log("Connected to Discord")
+    await mongo().then(function(mongoose) {
+        console.log('Connected to Mongo')
+        await mongoose.connection.close()
+    })
     if (config.activity.streaming) await client.user.setActivity(config.activity.game, { type: 'STREAMING', url: "https://twitch.tv/HibikiSpring" })
     else await client.user.setActivity(config.activity.game, { type: 'WATCHING' })
     
